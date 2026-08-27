af6900102f095152fee73c1b14ffa5aa710f34cb
JAVAIU194 <electriqladen@gmail.com>
2026-08-27 16:03:52 +0200

    Spring Boot Grundgerüst hinzugefügt

---
+++ src/main/java/com/likehero/like_hero_to_zero/LikeHeroToZeroApplication.java
@@ -0,0 +1,13 @@
+package com.likehero.like_hero_to_zero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeHeroToZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikeHeroToZeroApplication.class, args);
+	}
+
+}
